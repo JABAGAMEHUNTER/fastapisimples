4145d0d64efe9d24fa52000d66e45108dee21153
jaba <jmatheo71@gmail.com>
2024-06-17 12:53:23 -0400

    api completa

--- db/db_setup.py
+++ db/db_setup.py
@@ -3,8 +3,8 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://gwen@localhost/fast_lms"
-ASYNC_SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://gwen@localhost/fast_lms"
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres@localhost/fast_lms"
+ASYNC_SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://postgres@localhost/fast_lms"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={}, future=True)
 async_engine = create_async_engine(ASYNC_SQLALCHEMY_DATABASE_URL)
